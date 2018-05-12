299ce011e11de5f9fd850d3c00cbe87959dff70b
Emre Tanrıverdi <tanriverdi.emre2@gmail.com>
2018-05-12 15:56:03 +0300

    Added support for a second team.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -10,7 +10,9 @@ public class MainActivity extends AppCompatActivity {
     final int THREE_POINTS = 3;
     final int TWO_POINTS = 2;
     final int FREE_THROW = 1;
+
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void displayForTeamA(int score) {
         TextView scoreView = findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 
@@ -38,4 +45,18 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamA(scoreTeamA);
     }
 
+    public void addThreePointsForTeamB(View view) {
+        scoreTeamB += THREE_POINTS;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addTwoPointsForTeamB(View view) {
+        scoreTeamB += TWO_POINTS;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addOnePointForTeamB(View view) {
+        scoreTeamB += FREE_THROW;
+        displayForTeamB(scoreTeamB);
+    }
 }
